547da1f6ef5cc5410f4b6ec5f7da2278fc22dca3
Ian Liu Rodrigues <ian.liu88@gmail.com>
2020-09-30 14:31:18 -0300

    Limit number of windows in Xmonad to 3

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,12 +3,14 @@ import System.IO
 import XMonad
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.LimitWindows
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Layout.Spiral
-import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.EZConfig (additionalKeys, removeKeys)
 import XMonad.Util.Paste
 import XMonad.Util.Run (spawnPipe)
 
@@ -17,10 +19,11 @@ modKey = mod4Mask
 
 myManageHook :: ManageHook
 myManageHook = composeAll
-    [ title     =? "Microsoft Teams Notification" --> doFloat
+    [ title     =? "Microsoft Teams Notification" --> doIgnore <+> doFloat
     , className =? "Zenity"                       --> doFloat
-    , className =? "Microsoft Teams - Preview"    --> doShift "9"
+--  , className =? "Microsoft Teams - Preview"    --> doShift "9"
     , className =? "Steam"                        --> doShift "8"
+    , className =? "csgo_linux64"                 --> doShift "8"
     , wmRole    =? "gimp-message-dialog"          --> doFloat
     , wmRole    =? "gimp-toolbox-color-dialog"    --> doFloat
     ]
@@ -28,15 +31,22 @@ myManageHook = composeAll
             wmRole = stringProperty "WM_WINDOW_ROLE"
 
 gaps size = spacingRaw False (Border size 0 size 0) True (Border 0 size 0 size) True
-myLayouts = (gaps 10 $ Tall 1 (3/100) (1/2)) ||| (noBorders Full)
+myLayouts = (gaps 10 . limitWindows 3 $ Tall 1 (3/100) (1/2)) ||| (noBorders Full)
+
+xK_AudioLower = 0x1008FF11
+xK_AudioMute  = 0x1008FF12
+xK_AudioRaise = 0x1008FF13
+
+myRemoveKeys config =
+    removeKeys config [(modKey, xK_space)]
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad . docks $ def
+    xmonad . ewmh . docks . myRemoveKeys $ def
         { modMask            = modKey
         , borderWidth        = 2
         , focusedBorderColor = "#22D05F"
-        , terminal           = "termite"
+        , terminal           = "kitty"
         , focusFollowsMouse  = False
         , clickJustFocuses   = False
         , layoutHook         = avoidStruts $ myLayouts
@@ -46,13 +56,16 @@ main = do
             , ppTitle = xmobarColor "green" "" . shorten 50
             }
         } `additionalKeys`
-        [ ((modKey, xK_minus),  spawn "hide")
-        , ((modKey, xK_equal),  spawn "hide pop")
-        , ((modKey, xK_space),  spawn "dmenu_run")
-        , ((modKey, xK_f),      sendMessage NextLayout)
-        , ((modKey, xK_b),      sendMessage ToggleStruts)
-        , ((modKey, xK_grave),  toggleWS)
-        , ((0,      xK_Insert), pasteSelection)
+        [ ((modKey, xK_minus),      spawn "hide")
+        , ((modKey, xK_equal),      spawn "hide pop")
+--      , ((modKey, xK_space),      spawn "dmenu_run")
+        , ((modKey, xK_f),          sendMessage NextLayout >> sendMessage ToggleStruts)
+        , ((modKey, xK_p),          spawn "dmenu_run")
+        , ((modKey, xK_grave),      toggleWS)
+        , ((0,      xK_Insert),     pasteSelection)
+        , ((0,      xK_AudioLower), spawn "vol dec")
+        , ((0,      xK_AudioMute),  spawn "vol toggle")
+        , ((0,      xK_AudioRaise), spawn "vol inc")
         ]
 
 -- vim: set sw=4 et sta:
